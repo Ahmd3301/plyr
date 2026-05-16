68682d7da739f01be1c51504f9ab1ac9f305f424
احمد يسرى محمد محمود سيد <473309462@giza1.moe.edu.eg>
2026-05-17 01:34:49 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+android {
+    namespace = "io.videoplyr.app"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "io.videoplyr.app"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        debug { isDebuggable = true }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+}
